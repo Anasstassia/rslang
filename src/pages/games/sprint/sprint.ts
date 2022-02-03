f2e07b7fcf2250b85432ac89437ea7e41e2e9372
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2022-02-03 19:17:12 +0200

    feat: add basic sprint-game listeners

--- src/pages/games/sprint/sprint.ts
+++ src/pages/games/sprint/sprint.ts
@@ -3,11 +3,52 @@ import html from './sprint.html';
 import './sprint.scss';
 
 export class SprintGame implements content {
+  isSoundOn = false;
+
   async render() {
     return html;
   }
 
   async run() {
-    return undefined;
+    this.addListeners();
+  }
+
+  addListeners() {
+    const path = '../../../assets/icons';
+
+    // toggle sound
+    const soundBtn = document.getElementById('sprintSound') as HTMLImageElement;
+    soundBtn.addEventListener('click', () => {
+      this.isSoundOn = !this.isSoundOn;
+
+      if (this.isSoundOn) {
+        soundBtn.src = `${path}/soundOn.svg`;
+      } else {
+        soundBtn.src = `${path}/soundOff.svg`;
+      }
+    });
+
+    // toggle fullscreen
+    const fullscreenBtn = document.getElementById('sprintFullscreen') as HTMLImageElement;
+    fullscreenBtn.addEventListener('click', () => {
+      const sprintContent = document.querySelector('.sprint__content') as HTMLElement;
+
+      if (!document.fullscreenElement) {
+        document
+          .querySelector('.sprint')
+          ?.requestFullscreen()
+          .catch((err) => {
+            console.log(err.message);
+          });
+
+        sprintContent.style.marginTop = `calc(50% - ${sprintContent.clientHeight}px)`;
+
+        fullscreenBtn.src = `${path}/fullscreenOff.svg`;
+      } else {
+        document.exitFullscreen();
+        fullscreenBtn.src = `${path}/fullscreenOn.svg`;
+        sprintContent.style.marginTop = '';
+      }
+    });
   }
 }
