c547aed21054fa6f27880bf448f1c5abe418dd65
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2022-02-05 17:38:34 +0200

    feat: update UI and add animations

--- src/pages/games/sprint/sprint.ts
+++ src/pages/games/sprint/sprint.ts
@@ -1,9 +1,12 @@
+/* eslint-disable no-param-reassign */
 import { content } from '../../../core/components/types';
 import html from './sprint.html';
 import './sprint.scss';
 
 export class SprintGame implements content {
-  isSoundOn = false;
+  isSoundOn = true;
+
+  timeLeft = 60;
 
   async render() {
     return html;
@@ -20,12 +23,7 @@ export class SprintGame implements content {
     const soundBtn = document.getElementById('sprintSound') as HTMLImageElement;
     soundBtn.addEventListener('click', () => {
       this.isSoundOn = !this.isSoundOn;
-
-      if (this.isSoundOn) {
-        soundBtn.src = `${path}/soundOn.svg`;
-      } else {
-        soundBtn.src = `${path}/soundOff.svg`;
-      }
+      soundBtn.src = this.isSoundOn ? `${path}/soundOn.svg` : `${path}/soundOff.svg`;
     });
 
     // toggle fullscreen
@@ -34,15 +32,8 @@ export class SprintGame implements content {
       const sprintContent = document.querySelector('.sprint__content') as HTMLElement;
 
       if (!document.fullscreenElement) {
-        document
-          .querySelector('.sprint')
-          ?.requestFullscreen()
-          .catch((err) => {
-            console.log(err.message);
-          });
-
-        sprintContent.style.marginTop = `calc(50% - ${sprintContent.clientHeight}px)`;
-
+        document.querySelector('.sprint')?.requestFullscreen();
+        sprintContent.style.marginTop = '20vh';
         fullscreenBtn.src = `${path}/fullscreenOff.svg`;
       } else {
         document.exitFullscreen();
@@ -50,5 +41,95 @@ export class SprintGame implements content {
         sprintContent.style.marginTop = '';
       }
     });
+
+    document.getElementById('startSprintGameBtn')?.addEventListener('click', () => {
+      this.startGame();
+    });
+  }
+
+  startGame() {
+    const { time, line } = this.generateTimerUI();
+    const { word, translatedWord /* wrongBtn, correctBtn, mark */ } = this.generateContentUI();
+
+    this.generateWord(word, translatedWord);
+
+    this.startTimer(time, line);
+  }
+
+  startTimer(time: HTMLElement, line: HTMLElement) {
+    console.log(time, line);
+  }
+
+  generateWord(word: HTMLElement, translatedWord: HTMLElement) {
+    console.log(word, translatedWord);
+  }
+
+  generateContentUI() {
+    const word = document.createElement('h2');
+    word.classList.add('sprint__content__word');
+
+    word.innerHTML = 'Test'; // TODO: Remove
+
+    const translatedWord = document.createElement('p');
+    translatedWord.classList.add('sprint__content__translate');
+
+    translatedWord.innerText = 'Тест'; // TODO: Remove
+
+    const btnsWrap = document.createElement('div');
+    btnsWrap.classList.add('answer-btns');
+
+    const wrongBtn = document.createElement('button');
+    wrongBtn.classList.add('answer-btns__wrong');
+    wrongBtn.innerHTML = 'Не верно';
+
+    const correctBtn = document.createElement('button');
+    correctBtn.classList.add('answer-btns__correct');
+    correctBtn.innerHTML = 'Верно';
+
+    const mark = document.createElement('img');
+    mark.classList.add('answer-btns__mark', 'hide');
+    mark.alt = 'mark';
+
+    btnsWrap.append(wrongBtn, mark, correctBtn);
+
+    const wrap = document.querySelector('.sprint__content') as HTMLElement;
+
+    this.animate(wrap, 'changeContent', '3s', 'ease-in-out');
+    this.animate(word, 'appearanceContent', '3.2s', 'ease-in-out');
+    this.animate(translatedWord, 'appearanceContent', '3.2s', 'ease-in-out');
+    this.animate(btnsWrap, 'appearanceContent', '3.2s', 'ease-in-out');
+
+    wrap.innerHTML = '';
+    wrap.append(word, translatedWord, btnsWrap);
+
+    return { word, translatedWord, wrongBtn, correctBtn, mark };
+  }
+
+  generateTimerUI() {
+    const timerWrap = document.createElement('div');
+    timerWrap.classList.add('timer');
+
+    const time = document.createElement('span');
+    time.innerHTML = this.timeLeft.toString();
+
+    const line = document.createElement('div');
+    line.classList.add('line');
+
+    timerWrap.append(time, line);
+
+    const wrap = document.querySelector('.sprint .container') as HTMLElement;
+
+    this.animate(timerWrap, 'timerAppearance', '3s', 'ease-in-out');
+
+    wrap.appendChild(timerWrap);
+
+    return { time, line };
+  }
+
+  animate(element: HTMLElement, name: string, time: string, animationFunc: string) {
+    element.style.animationName = `${name}`;
+    element.style.animationDuration = `${time}`;
+    element.style.animationTimingFunction = `${animationFunc}`;
+    element.style.animationFillMode = 'forwards';
   }
 }
