003299351d5382236e915ca2ccfb4b37a983d1e0
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2022-02-13 17:14:32 +0200

    fix: remove timer

--- src/pages/games/audioCall/audioCall.ts
+++ src/pages/games/audioCall/audioCall.ts
@@ -1,12 +1,10 @@
 import { content } from '../../../core/components/types';
-import { changeContent, show } from '../animation';
+import { changeContent } from '../animation';
 import { checkLocalStarage, toggleHeaderBtns } from '../utils';
 import html from './audioCall.html';
 import './audioCall.scss';
 
 export class AudioCall implements content {
-  timeLeft = 10;
-
   async render() {
     return html;
   }
@@ -31,66 +29,7 @@ export class AudioCall implements content {
   }
 
   startGame() {
-    const { time, line } = this.generateTimerUI();
     this.generateContentUI();
-
-    this.startTimer(time, line, 3000);
-  }
-
-  generateTimerUI() {
-    const timerWrap = document.createElement('div');
-    timerWrap.classList.add('timer');
-
-    const time = document.createElement('span');
-    time.innerHTML = this.timeLeft.toString();
-
-    const line = document.createElement('div');
-    line.classList.add('line');
-
-    timerWrap.append(time, line);
-
-    const wrap = document.querySelector('.audio-call .container') as HTMLElement;
-
-    show(timerWrap, 3000, 300, 0.7);
-
-    wrap.appendChild(timerWrap);
-
-    return { time, line };
-  }
-
-  startTimer(time: HTMLElement, line: HTMLElement, delay: number) {
-    setTimeout(() => {
-      let width = line.clientWidth;
-      line.style.width = `${width}px`;
-
-      const interval = setInterval(() => {
-        this.timeLeft -= 1;
-        time.innerHTML = this.timeLeft.toString();
-
-        width -= 30;
-        line.style.width = `${width}px`;
-
-        if (this.timeLeft === 5) {
-          time.style.color = '#ffe600';
-          line.style.backgroundColor = '#ffe600';
-        }
-
-        if (this.timeLeft === 3) {
-          time.style.color = '#bd0404';
-          line.style.backgroundColor = '#bd0404';
-        }
-
-        if (this.timeLeft === 0) {
-          console.log('Next word');
-
-          clearInterval(interval);
-
-          this.timeLeft = 10;
-          time.style.color = '';
-          line.style.backgroundColor = '';
-        }
-      }, 1000);
-    }, delay);
   }
 
   generateContentUI() {
